3c955bbf5d4227e342f3c2404339218d3a1bb64e
Teresa Fan <teresaefan@gmail.com>
2014-03-25 22:11:51 -0400

    Merge branch 'master' of https://github.com/stefaniea/WomensHealth_CIS350
    Conflicts:
    	bin/WomensHealth_CIS350.apk
    	bin/classes.dex
    	bin/classes/com/example/womenshealth_cis350/LetsGetStarted.class
    	bin/classes/com/example/womenshealth_cis350/R$id.class
    	bin/classes/com/example/womenshealth_cis350/R$layout.class
    	bin/classes/com/example/womenshealth_cis350/R$menu.class
    	bin/classes/com/example/womenshealth_cis350/R$string.class
    	bin/classes/com/example/womenshealth_cis350/R$style.class
    	bin/classes/com/example/womenshealth_cis350/WhatToExpect.class
    	bin/resources.ap_
    	gen/com/example/womenshealth_cis350/R.java

--- gen/com/example/womenshealth_cis350/R.java
+++ gen/com/example/womenshealth_cis350/R.java
@@ -188,33 +188,53 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int btn_dropdown=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+<<<<<<< HEAD
+        public static final int action_settings=0x7f0a0010;
+        public static final int age_content=0x7f0a000f;
+        public static final int age_spinner=0x7f0a000e;
+=======
+<<<<<<< HEAD
+        public static final int LinearLayout01=0x7f0a000b;
+        public static final int ScrollView01=0x7f0a000d;
+        public static final int action_settings=0x7f0a000f;
+        public static final int age_content=0x7f0a000e;
+        public static final int age_spinner=0x7f0a000c;
+=======
         public static final int action_settings=0x7f0a000f;
         public static final int age_content=0x7f0a000e;
         public static final int age_spinner=0x7f0a000d;
+>>>>>>> 465343103260a98ca1198475793a60ac1e73c0b8
+>>>>>>> 2c692290789fbc57a5dfa5e541e6f9db24f77e24
         public static final int button1=0x7f0a0002;
         public static final int button2=0x7f0a0003;
-        public static final int checkBox1=0x7f0a0007;
-        public static final int checkBox2=0x7f0a0006;
-        public static final int checkBox3=0x7f0a0008;
-        public static final int checkBox4=0x7f0a0009;
-        public static final int checkBox5=0x7f0a000a;
-        public static final int checkBox6=0x7f0a000b;
-        public static final int checkBox7=0x7f0a000c;
-        public static final int getstarted=0x7f0a0004;
+        public static final int button3=0x7f0a0004;
+        public static final int checkBox1=0x7f0a0008;
+        public static final int checkBox2=0x7f0a0007;
+        public static final int checkBox3=0x7f0a0009;
+        public static final int checkBox4=0x7f0a000a;
+        public static final int checkBox5=0x7f0a000b;
+        public static final int checkBox6=0x7f0a000c;
+        public static final int checkBox7=0x7f0a000d;
+        public static final int getstarted=0x7f0a0005;
         public static final int subtitle=0x7f0a0001;
-        public static final int textView1=0x7f0a0005;
+        public static final int textView1=0x7f0a0006;
         public static final int title_screen=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int drawable=0x7f030001;
-        public static final int get_started=0x7f030002;
-        public static final int latching_correctly=0x7f030003;
-        public static final int what_to_expect=0x7f030004;
+        public static final int benefits=0x7f030001;
+        public static final int drawable=0x7f030002;
+        public static final int get_started=0x7f030003;
+        public static final int latching_correctly=0x7f030004;
+<<<<<<< HEAD
+        public static final int what_to_expect=0x7f030005;
+=======
+        public static final int pregnant_postpartum=0x7f030005;
+        public static final int what_to_expect=0x7f030006;
+>>>>>>> 2c692290789fbc57a5dfa5e541e6f9db24f77e24
     }
     public static final class menu {
         public static final int main=0x7f090000;
@@ -267,7 +287,5 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f080001;
-        public static final int Our_Theme=0x7f080002;
-        public static final int Widget_Spinner=0x7f080003;
     }
 }
