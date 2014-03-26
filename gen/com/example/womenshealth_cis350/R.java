05a225dd4d9f27c79bb3c781b60f014d6672a086
Rebecca Hallac <rhallac@seas.upenn.edu>
2014-03-25 20:48:18 -0400

    Added checkbox!
    baby latching

--- gen/com/example/womenshealth_cis350/R.java
+++ gen/com/example/womenshealth_cis350/R.java
@@ -28,18 +28,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
-        public static final int age_content=0x7f090005;
-        public static final int age_spinner=0x7f090004;
+        public static final int action_settings=0x7f09000d;
+        public static final int age_content=0x7f09000c;
+        public static final int age_spinner=0x7f09000b;
         public static final int button1=0x7f090002;
-        public static final int getstarted=0x7f090003;
+        public static final int button2=0x7f090003;
+        public static final int checkBox1=0x7f090007;
+        public static final int checkBox2=0x7f090006;
+        public static final int checkBox3=0x7f090008;
+        public static final int checkBox4=0x7f090009;
+        public static final int checkBox5=0x7f09000a;
+        public static final int editText1=0x7f090005;
+        public static final int getstarted=0x7f090004;
         public static final int subtitle=0x7f090001;
         public static final int title_screen=0x7f090000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int get_started=0x7f030001;
-        public static final int what_to_expect=0x7f030002;
+        public static final int latching_correctly=0x7f030002;
+        public static final int what_to_expect=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
