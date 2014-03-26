a849fc8afb0b55056f29dadc7f326afe0297d636
Stefanie Alfonso <astef@seas.upenn.edu>
2014-03-25 20:59:11 -0400

    Added colors (did the last commit work???)

--- gen/com/example/womenshealth_cis350/R.java
+++ gen/com/example/womenshealth_cis350/R.java
@@ -155,6 +155,8 @@ public final class R {
         public static final int Yellow=0x7f040014;
         public static final int YellowGreen=0x7f040061;
         public static final int aqua=0x7f04000a;
+        /**  COLORS IMPORTANT TO OUR APP 
+         */
         public static final int bgcolor=0x7f040000;
         public static final int black=0x7f040010;
         public static final int blue=0x7f04000e;
@@ -169,6 +171,8 @@ public final class R {
         public static final int red=0x7f040004;
         public static final int silver=0x7f040005;
         public static final int teal=0x7f04000c;
+        /**  OTHER RANDOM COLORS FROM THE INTERWEBZ 
+         */
         public static final int white=0x7f040001;
         public static final int yellow=0x7f040002;
     }
@@ -187,18 +191,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0006;
-        public static final int age_content=0x7f0a0005;
-        public static final int age_spinner=0x7f0a0004;
+        public static final int action_settings=0x7f0a000d;
+        public static final int age_content=0x7f0a000c;
+        public static final int age_spinner=0x7f0a000b;
         public static final int button1=0x7f0a0002;
-        public static final int getstarted=0x7f0a0003;
+        public static final int button2=0x7f0a0003;
+        public static final int checkBox1=0x7f0a0007;
+        public static final int checkBox2=0x7f0a0006;
+        public static final int checkBox3=0x7f0a0008;
+        public static final int checkBox4=0x7f0a0009;
+        public static final int checkBox5=0x7f0a000a;
+        public static final int editText1=0x7f0a0005;
+        public static final int getstarted=0x7f0a0004;
         public static final int subtitle=0x7f0a0001;
         public static final int title_screen=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int get_started=0x7f030001;
-        public static final int what_to_expect=0x7f030002;
+        public static final int latching_correctly=0x7f030002;
+        public static final int what_to_expect=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f090000;
