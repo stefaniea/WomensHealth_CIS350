e427d23393801ef9bca9873b4b6fb3bc5f6f3126
Rebecca Hallac <rhallac@seas.upenn.edu>
2014-03-25 22:33:54 -0400

    test please work

--- gen/com/example/womenshealth_cis350/R.java
+++ gen/com/example/womenshealth_cis350/R.java
@@ -216,7 +216,8 @@ public final class R {
         public static final int drawable=0x7f030002;
         public static final int get_started=0x7f030003;
         public static final int latching_correctly=0x7f030004;
-        public static final int what_to_expect=0x7f030005;
+        public static final int pregnant_postpartum=0x7f030005;
+        public static final int what_to_expect=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f090000;
