cda1a8f3135d62f0a3e8fda577b680e7d27368d4
Stefanie Alfonso <astef@seas.upenn.edu>
2014-04-06 18:22:34 -0400

    Added bubble in BabyGainingWeight

--- src/com/example/womenshealth_cis350/BabyGainingWeight.java
+++ src/com/example/womenshealth_cis350/BabyGainingWeight.java
@@ -8,12 +8,12 @@ import android.view.View;
 import android.widget.TextView;
 
 public class BabyGainingWeight extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.gaining_weight);
-		TextView content = (TextView) findViewById(R.id.weight_content);
-		content.setText(R.string.gain_weight);
+		
 	}
 	
 	
